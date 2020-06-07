c60bc787746af46d854c95a72708d29762934d2d
Eliel de Paula <elieldepaula@gmail.com>
2020-06-07 20:49:35 -0300

    Arquivos incompletos.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+
+    /**
+     * @var Request
+     */
+    public $request;
+
+    function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function input($key = '')
+    {
+        return $this->request->input($key);
+    }
+
+    /**
+     * Set response for the service.
+     *
+     * @param $body Body of the response.
+     * @param int $code Status code
+     * @param bool $success Succcess status true|false
+     * @return array
+     */
+    public function setResponse($body, int $code = 200, bool $success = true) : array
+    {
+        return ['success' => $success, 'code' => $code, 'body' => $body];
+    }
+
+    /**
+     * Set error response for the service.
+     *
+     * @todo Verificar se não é melhor remover a tipagem da Exception para algo mais flexível.
+     *
+     * @param \Exception $error Exception object.
+     * @param bool $success Success status true|false
+     * @return array
+     */
+    public function setError(\Exception $error, bool $success = true) : array
+    {
+        return ['success' => $success, 'error' => ['code' => $error->getCode(), 'message' => $error->getMessage()]];
+    }
+
 }
